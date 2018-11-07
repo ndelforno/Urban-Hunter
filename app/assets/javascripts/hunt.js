35566c565c2a2939d83d1ab66a3535ff16fa0bb9
Ndelforno <nicolas.delforno@gmail.com>
2018-11-07 18:05:14 -0500

    change profile compare to schema

--- app/assets/javascripts/hunt.js
+++ app/assets/javascripts/hunt.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-// // //
-var mymapAll = L.map('mapid_all').setView([43.6532, -79.3832], 13);
-var taskaddresses = document.querySelectorAll(".taskaddress")
-// // //   // var addressText = address.innerText;
-// // //
+
 if(document.getElementById('mapid_all')){
+  var mymapAll = L.map('mapid_all').setView([43.6532, -79.3832], 13);
+  var taskaddresses = document.querySelectorAll(".taskaddress")
+
+  console.log('not good');
   var searchAddresses = []
 
   for (var i = 0; i < taskaddresses.length; i++) {
@@ -30,18 +30,12 @@ if(document.getElementById('mapid_all')){
     });
   }
 
-
-
-
-
-
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       id: 'mapbox.streets',
       accessToken: 'pk.eyJ1IjoibmRlbGZvcm5vIiwiYSI6ImNqbzY2MzlyZTBoczUzcW5sc2k3dGFsZ2YifQ.JL97VOzlsaPc4uDrUwlAnw'
     }).addTo(mymapAll);
-
   }
 
 });
