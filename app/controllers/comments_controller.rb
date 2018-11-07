6f89308edce8deb519da18911bc791a634a2c37f
kevintudiep <kevintudiep@gmail.com>
2018-11-07 10:44:40 -0600

    work in progress

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,17 @@ class CommentsController < ApplicationController
   end
 
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new
+
+    @comment.description = params[:comment][:description]
+    @comment.user = current_user
+    @comment.hunt = Hunt.find(params[:hunt_id])
+
+    @comment.save
   end
 
   def show
