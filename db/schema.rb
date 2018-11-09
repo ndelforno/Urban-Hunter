440cbb1fcbc044772040f30c1ff274cb6442e747
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-11-09 15:41:11 -0500

    User sign_up error_handling_done

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_11_08_154117) do
+ActiveRecord::Schema.define(version: 2018_11_08_181331) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
