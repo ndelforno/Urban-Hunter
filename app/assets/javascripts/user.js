7147d918bbeabc27656d1b3fa44993217a4fed7c
Ndelforno <nicolas.delforno@gmail.com>
2018-11-09 17:05:58 -0500

    fixed login bug form

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -4,11 +4,12 @@ if(document.querySelector(".sign_in")){
 var signin = document.querySelector(".sign_in");
 var sign_up_form = document.querySelector(".sign_up_form");
 var close = document.querySelector(".close_sign_up");
+var log_in_form = document.querySelector(".log_in_form");
 
 signin.addEventListener('click', function(event){
   event.preventDefault();
   sign_up_form.style.display = "block";
-
+  log_in_form.style.display = "none";
 });
 
 close.addEventListener('click', function(){
