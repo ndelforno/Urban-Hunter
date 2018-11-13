eb2d397a10c5a6085246d246389a87e2b2b0a010
costagavras <cgavras@yahoo.ca>
2018-11-13 11:12:00 -0500

    nav_bar_on_top_some_CSS

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,14 +1,14 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "350px";
-    document.getElementById("main_to_slide").style.marginLeft = "350px";
+    // document.getElementById("main_to_slide").style.marginLeft = "350px";
     document.getElementById("open_button").style.display = "none";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main_to_slide").style.marginLeft = "0";
+    // document.getElementById("main_to_slide").style.marginLeft = "0";
     document.getElementById("open_button").style.display = "block";
 }
 
