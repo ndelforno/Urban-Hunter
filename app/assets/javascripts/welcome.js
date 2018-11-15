f42512eed4db25bfd5993e6ae55f427cdb937ab5
costagavras <cgavras@yahoo.ca>
2018-11-15 16:35:55 -0500

    I18n_index_page_yml_category

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,4 +1,29 @@
+/*cookie code from https://www.quirksmode.org/js/cookies.html*/
 
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
@@ -16,59 +41,48 @@ function closeNav() {
 
 /*translate categories labels on link click*/
 
-// document.addEventListener("DOMContentLoaded", function(){
-//
-//   var enLinkLang = document.getElementById("en");
-//   var itLinkLang = document.getElementById("it");
-//   var frLinkLang = document.getElementById("fr");
-//   var jpLinkLang = document.getElementById("jp");
-//   var deLinkLang = document.getElementById("de");
-//   // console.log(itLinkLang.id);
-//
-//   enLinkLang.addEventListener('click', function(){
-//         translateCategories(this.id);
-//   });
-//   itLinkLang.addEventListener('click', function(){
-//         translateCategories(this.id);
-//   });
-// });
-//
-// /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-// function translateCategories(lang) {
-//     var arEnLang = ["TOURISM", "SOCIAL", "PUB CRAWL", "FAMILY", "SPORT"];
-//     var arItLang = ["TURISMO", "SOCIALE", "PUB CRAWL", "FAMIGLIA", "SPORT"];
-//     var arFrLang = ["TOURISME", "SOCIALE", "PUB CRAWL", "FAMILLE", "SPORT"];
-//     var arJpLang = ["TOURISM", "SOCIAL", "PUB CRAWL", "FAMILY", "SPORT"];
-//     var arDeLang = ["TOURISMUS", "SOZIAL", "KNEIPENTOUR", "FAMILIE", "SPORT"];
-//     // console.log(lang);
-//     switch(lang) {
-//       case "en":
-//         var arNewCat = arEnLang;
-//         break;
-//       case "it":
-//         var arNewCat = arItLang;
-//         break;
-//       case "fr":
-//         var arNewCat = arFrLang;
-//         break;
-//       case "jp":
-//         var arNewCat = arJpLang;
-//         break;
-//       case "de":
-//         var arNewCat = arDeLang;
-//         break;
-//       default:
-//         var arNewCat = arEnLang;
-//     };
-//     // return arNewCat;
-//     // console.log(arNewCat);
-//     var arCategories = document.querySelectorAll('.category_names');
-//     // console.log(arCategories[0].innerHTML);
-//     var iCatLength = arNewCat.length;
-//     for (var i = 0; i < iCatLength; i++){
-//       arCategories[i].innerText = arNewCat[i];
-//     };
-// };
+document.addEventListener("DOMContentLoaded", function(){
+  var lang = readCookie("lang_locale")
+  translateCategories(lang);
+
+});
+
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function translateCategories(lang) {
+    var arEnLang = ["TOURISM", "SOCIAL", "PUB CRAWL", "FAMILY", "SPORT"];
+    var arItLang = ["TURISMO", "SOCIALE", "PUB CRAWL", "FAMIGLIA", "SPORT"];
+    var arFrLang = ["TOURISME", "SOCIALE", "PUB CRAWL", "FAMILLE", "SPORT"];
+    var arJpLang = ["TOURISM", "SOCIAL", "PUB CRAWL", "FAMILY", "SPORT"];
+    var arDeLang = ["TOURISMUS", "SOZIAL", "KNEIPENTOUR", "FAMILIE", "SPORT"];
+    // console.log(lang);
+    switch(lang) {
+      case "en":
+        var arNewCat = arEnLang;
+        break;
+      case "it":
+        var arNewCat = arItLang;
+        break;
+      case "fr":
+        var arNewCat = arFrLang;
+        break;
+      case "jp":
+        var arNewCat = arJpLang;
+        break;
+      case "de":
+        var arNewCat = arDeLang;
+        break;
+      default:
+        var arNewCat = arEnLang;
+    };
+
+    var arCategories = document.querySelectorAll('.category_names');
+    // console.log(arCategories[0].innerHTML);
+    var iCatLength = arNewCat.length;
+    for (var i = 0; i < iCatLength; i++){
+      arCategories[i].innerText = arNewCat[i];
+    };
+};
 
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
