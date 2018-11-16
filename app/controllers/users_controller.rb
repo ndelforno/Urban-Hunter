270f05c372d3aa85d3afed15a89f3db62ade83f1
Ndelforno <nicolas.delforno@gmail.com>
2018-11-16 10:00:23 -0500

    added email & password to edit user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.first_name = params[:user][:first_name]
     @user.last_name = params[:user][:last_name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
     if @user.save
       redirect_to user_path(current_user)
     else
