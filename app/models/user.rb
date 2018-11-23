c082debe2e5fa2a629b4bb6307f87f8441eaab26
costagavras <cgavras@yahoo.ca>
2018-11-23 12:50:11 -0500

    profile_pics_all

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :participations
+  has_many :participations, dependent: :destroy
   has_many :participated_hunts, through: :participations, source: :hunt
-  has_many :completed_tasks
+  has_many :completed_tasks, dependent: :destroy
   has_many :tasks, through: :completed_tasks
   has_many :hunts #owned_hunt
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :first_name, :last_name, presence: true
   validates :password, confirmation: true, on: :create
